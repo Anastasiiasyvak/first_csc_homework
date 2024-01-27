9b9d4bf267497dc4a7d20843211fcecbe60472e0
Anastasiiasyvak <asyvak@kse.org.ua>
2024-01-28 00:14:50 +0200

    client.cpp + server.cpp

--- server.cpp
+++ server.cpp
@@ -1,5 +1,57 @@
-//
-// Created by nastia on 24.01.24.
-//
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-#include "server.h"
+int main() {
+    // Server configuration
+    int port = 12345;
+    // Create a TCP socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        perror("Error creating socket");
+        return 1;
+    }
+    // Bind the socket to a specific address and port
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY; // вказує, що сервер буде приймати з'єднання на будь-який зареєстрований IP-адрес цього пристрою
+    serverAddr.sin_port = htons(port); //  використовується для перетворення порту з локального порядку байтів (залежить від архітектури) в мережевий порядок байтів
+    if (bind(serverSocket, reinterpret_cast<struct sockaddr*>(&serverAddr), sizeof(serverAddr)) == -1) {
+        perror("Bind failed");
+        close(serverSocket);
+        return 1;
+    }
+    // Listen for incoming connections
+    if (listen(serverSocket, SOMAXCONN) == -1) {
+        perror("Listen failed");
+        close(serverSocket);
+        return 1;
+    }
+    std::cout << "Server listening on port " << port << std::endl;
+    // Accept a client connection
+    sockaddr_in clientAddr{};
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    int clientSocket = accept(serverSocket, reinterpret_cast<struct sockaddr*>(&clientAddr), &clientAddrLen);
+    if (clientSocket == -1) {
+        perror("Accept failed");
+        close(serverSocket);
+        return 1;
+    }
+    std::cout << "Accepted connection from " << inet_ntoa(clientAddr.sin_addr) << ":" << ntohs(clientAddr.sin_port) <<
+              std::endl;
+    // Receive data from the client
+    char buffer[1024];
+    memset(buffer, 0, 1024);
+    ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesReceived > 0) {
+        std::cout << "Received data: " << buffer << std::endl;
+        // Send a response back to the client
+        const char* response = "Hello, client! This is the server.";
+        send(clientSocket, response, strlen(response), 0);
+    }
+    // Clean up
+    close(clientSocket);
+    close(serverSocket);
+    return 0;
+}
